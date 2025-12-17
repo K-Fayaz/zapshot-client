f0afbcd0b047f01faf1daef14f08d01860d2554f
Fayaz <kfayaz1407@gmail.com>
2025-12-17 12:22:00 +0530

    updated base url for render backend web service

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 const DEV_BASE = "http://localhost:8080/";
 const PROD_BASE="/";
+const RENDER_BASE="https://zapshot-server.onrender.com/"
 
-const BASE_URL = PROD_BASE;
+const BASE_URL = RENDER_BASE;
 
 export default BASE_URL;

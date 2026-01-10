273ad9b94448a76153ac9d043fb92786d143b380
Fayaz <kfayaz1407@gmail.com>
2026-01-10 12:52:34 +0530

    feat: added vercel server base url

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const DEV_BASE = "http://localhost:8080/";
 const PROD_BASE="/";
-const RENDER_BASE="https://zapshot-server.onrender.com/"
+const RENDER_BASE="https://zapshot-server-m4wxht3xj-fayazs-projects-07c3232e.vercel.app"
 
 const BASE_URL = RENDER_BASE;
 

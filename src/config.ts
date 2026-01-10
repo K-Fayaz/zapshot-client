76c637a01d529d5e0ec6b17161583721b28bc8a9
Fayaz <kfayaz1407@gmail.com>
2026-01-10 13:22:58 +0530

    fix: use render server url

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const DEV_BASE = "http://localhost:8080/";
 const PROD_BASE="/";
-const RENDER_BASE="https://zapshot-server-m4wxht3xj-fayazs-projects-07c3232e.vercel.app/"
+const RENDER_BASE="https://zapshot-server.onrender.com/"
 
 const BASE_URL = RENDER_BASE;
 
